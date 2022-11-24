43846f23850f9acca8caf9f21f90173fc3be347b
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2022-11-24 16:13:09 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('olá mundo');
+import fs from "fs";
+import chalk from "chalk";
+
+function extrairLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2] }));
+  return resultados.length !== 0 ? resultados : chalk.red("Não possui links");
+}
+
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, "Não há arquivo no diretório"));
+}
+
+async function pegaArquivo(caminho) {
+  try {
+    const encoding = "utf-8";
+    const texto = await fs.promises.readFile(caminho, encoding);
+    return extrairLinks(texto);
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
+pegaArquivo('./arquivos/texto.md');
+
+export default pegaArquivo;
