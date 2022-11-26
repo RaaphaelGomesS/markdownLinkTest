5bedbaef0cd91647640204eb96b337b55e949f11
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2022-11-26 16:42:17 -0300

    feat: checa status dos links e adciona ao objeto

--- src/http-validação.js
+++ src/http-validação.js
@@ -1,11 +1,43 @@
-function extrairLinks(arr){
-    return arr.map((objeto) => {
-        return Object.values(objeto).join();
-    })
+import chalk from "chalk";
+
+function extrairLinks(arr) {
+  return arr.map((objeto) => {
+    return Object.values(objeto).join();
+  });
 }
 
-function listaValidada(listaDeLinks){
-    return extrairLinks(listaDeLinks);
+async function checaStatus(urlLista) {
+  const arrStatus = await Promise.all(
+    urlLista.map(async (url) => {
+      try {
+        const response = await fetch(url);
+        return `${response.status} - ${response.statusText}`;
+      } catch (error) {
+        return manejaErros(error);
+      }
+    })
+  );
+  return arrStatus;
+}
+
+function manejaErros(error) {
+  if (error.cause.code === "ENOTFOUND") {
+    return "link não encontrado";
+  } else {
+    return "Ocorreu algum erro";
+  }
+}
+
+async function listaValidada(listaDeLinks) {
+  const links = extrairLinks(listaDeLinks);
+  const status = await checaStatus(links);
+
+  return listaDeLinks.map((obj, indice) => 
+    ({
+      ...obj,
+      status: status[indice],
+    })
+  );
 }
 
 export default listaValidada;
